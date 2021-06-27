74deceb95649d2705203ee46a7b49832aacdefb1
amritp22 <81180708+amritp22@users.noreply.github.com>
2021-06-27 21:05:19 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Footer(){
+  return (
+  <Footer>  <p>
+Copyright by ShapeAI  @{new Date().getFullYear()}
+    </p>
+    <Footer>
+  );
+}
+
+export default Footer;
---
+++ Info.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Info(){
+  return (
+    <div class="note">
+<h1>WEB development and reactjs</h1>
+ <p> a bsic webdev and reactjs bootcamp</p>
+ </div>
+  );
+}
+
+export default Info;
